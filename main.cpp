4298692016a8eaeed50c921a4d91f8c25eecd86a
Rado Radev <rado@auxnederlandbv.nl>
2021-11-14 15:53:35 +0200

    Implement data structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
 #include "mainwindow.h"
 
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
 
+#include "array-vector.h"
+
 int main(int argc, char *argv[])
 {
+    ArrayVector<int> b;
+    b.insert(0, 5);
+    b.insert(0, 6);
+    b.insert_back(9);
+
+    cout << b.at(2) << endl;
+    b.erase_back();
+    cout << b.at(0) << endl;
+    b[0] = 5;
+    cout << b.at(0) << endl;
+
+    b.sort(ArrayVector<int>::SELECTION_SORT);
+
+
     QApplication a(argc, argv);
 
     QTranslator translator;
--- sorting-algorithms.pro
+++ sorting-algorithms.pro
@@ -13,6 +13,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    array-vector.h \
     mainwindow.h
 
 FORMS += \
